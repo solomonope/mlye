32321f4e101f09a7475bc0b9d365f55c8a9c75f1
s.folorunsho <s.folorunsho@mytaxi.com>
2017-08-20 12:43:45 +0200

    updates

--- features/bag_of_words.py
+++ features/bag_of_words.py
@@ -1,15 +1,40 @@
-from sklearn.feature_extraction.text import  CountVectorizer;
+from sklearn.feature_extraction.text import CountVectorizer;
+from sklearn.metrics.pairwise import euclidean_distances
+
 """A collection of documents"""
 corpus = [
-    "UNC played duke in basketball",
+    "UNC played Played duke in basketball",
     "Duke lost the basketball game",
     "I ate a sandwitch"
 ]
 
-vectorizer = CountVectorizer();
+vectorizer = CountVectorizer(stop_words="english");
 
 bow = vectorizer.fit_transform(corpus);
 dbow = bow.todense();
 
 print(vectorizer.vocabulary_)
 print(dbow);
+print(euclidean_distances(dbow[0], dbow[1]))
+print(euclidean_distances(dbow[0], dbow[2]))
+print(euclidean_distances(dbow[1], dbow[2]))
+
+corpus2 = [
+    "He ate the sandwitches",
+    "Every sandwitch was eaten by him"
+]
+
+vectorizer2 = CountVectorizer(binary=True, stop_words="english")
+
+bowz =  vectorizer2.fit_transform(corpus2).todense();
+
+print(bowz)
+print(vectorizer2.vocabulary_)
+print(euclidean_distances(bowz[0], bowz[1]))
+
+corpus3 = [
+    "I am gathering ingredients for the sandwitch",
+    "There were many wizards at the gathering"
+]
+
+import nltk;
